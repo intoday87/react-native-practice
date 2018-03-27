78815e811d7f25aeabdefc07261efe979265f032
이동호(Nick) <intoday87@coupang.com>
2018-03-27 22:54:19 +0900

    jest learning test

--- test/test.js
+++ test/test.js
@@ -11,10 +11,51 @@ Link.propTypes = {
   children: PropTypes.string.isRequired,
 };
 
-it('have a link of facebook', () => {
-  const testRenderer = renderer.create(<Link page="www.facebook.com">fuck you</Link>);
+describe('Link', () => {
 
-  expect(testRenderer.toJSON().type).toBe('a');
-  expect(testRenderer.toJSON().props.href).toBe('www.facebook.com');
-  expect(testRenderer.toJSON().children).toContain('fuck you');
+  it('have a link of facebook', () => {
+    const testRenderer = renderer.create(<Link page="www.facebook.com">fuck you</Link>);
+
+    expect(testRenderer.toJSON().type).toBe('a');
+    expect(testRenderer.toJSON().props.href).toBe('www.facebook.com');
+    expect(testRenderer.toJSON().children).toContain('fuck you');
+  });
+});
+
+describe('jest mock', () => {
+
+  const forEach = (items, callback) => {
+    for (let i = 0; i < items.length; ++i) {
+      callback(items[i], i);
+    }
+  }
+
+  it('spec name', () => {
+    const mockCallback = jest.fn();
+
+    forEach([0, 1], mockCallback);
+
+    expect(mockCallback.mock.calls.length).toBe(2);
+  });
+
+  it('spec name2', () => {
+    const mockCallback = jest.fn();
+
+    forEach([5, 1], mockCallback);
+
+    expect(mockCallback.mock.calls[0][0]).toBe(5);
+    expect(mockCallback.mock.calls[0][1]).toBe(0);
+  });
+
+  it('should ', () => {
+    const myMock = jest.fn();
+
+    const a = new myMock('fuck');
+    a.name = 'test name';
+
+    expect(myMock.mock.instances.length).toBe(1);
+    expect(myMock.mock.calls[0][0]).toEqual('fuck');
+    expect(myMock.mock.instances[0].name).toEqual('test name')
+  });
+
 });
