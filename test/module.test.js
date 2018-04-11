866502eb57b40536c9f699ef9300132201ee8d71
이동호(Nick) <intoday87@coupang.com>
2018-04-11 22:06:11 +0900

    jest의 es6 mocking에서 absolute import 보다 relative import가 선행될 수 있다

--- test/module.test.js
+++ test/module.test.js
@@ -1,22 +1,19 @@
 import Module from './module';
 import $ from 'jquery';
+
 jest.mock('jquery');
 
 describe('Module', () => {
-
   beforeEach(() => {
     $.mockClear();
   });
 
   it('should return jquery set', () => {
-    $.mockImplementation(() => {
-      return {a:1};
-    });
+    $.mockImplementation(() => ({ a: 1 }));
 
     const result = Module.get();
 
     expect($.mock.calls[0][0]).toBe('#a');
-    expect(result).toEqual({a:1});
+    expect(result).toEqual({ a: 1 });
   });
-
 });
