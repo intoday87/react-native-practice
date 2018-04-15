429faae3983b59f557fe6c0275bab5da904de78e
이동호(Nick) <intoday87@coupang.com>
2018-04-15 23:11:34 +0900

    DatePickerIOS example
    View style에 alignItems: 'center'가 들어가 있으면 나오지 않는다

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { DatePickerIOS, StyleSheet, Text, View } from 'react-native';
 import Bananas from './src/Bananas';
 import Greeting from './src/Greeting';
 import Blink from './src/Blink';
@@ -9,20 +9,39 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
 
-export default () => (
-  <View style={styles.container}>
-    <Text>Open up App.js to start working on your app!</Text>
-    <Text>Changes you make will automatically reload.</Text>
-    <Text>Shake your phone to open the developer menu.</Text>
-    <Text>Fuck</Text>
-    <Bananas />
-    <Greeting name="fucking name" />
-    <Blink text="blink" />
-    <LotOfStyles />
-  </View>
-);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { chosenDate: new Date() };
+
+    this.setDate = this.setDate.bind(this);
+  }
+
+  setDate(newDate) {
+    alert(newDate);
+    this.setState({ chosenDate: newDate });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Fuck</Text>
+        <Bananas />
+        <Greeting name="fucking name" />
+        <Blink text="blink" />
+        <LotOfStyles />
+        <DatePickerIOS
+          date={this.state.chosenDate}
+          onDateChange={this.setDate}
+        />
+      </View>
+    );
+  }
+}
