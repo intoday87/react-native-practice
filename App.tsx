7ed55afadd7da178d6a5dc2e13333c1f9bc61708
intoday87 <intoday_@naver.com>
2022-12-01 09:34:41 +0900

    Upgrade react-native version 0.70 and react-query debugger
    - add flipper plugin for react-query

--- App.tsx
+++ App.tsx
@@ -1,74 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {useRef} from 'react'
+import React from 'react'
 import {
-  Animated,
-  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
   View,
 } from 'react-native'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen'
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen'
+import Animation from './src/animation'
+import {QueryClient, QueryClientProvider} from 'react-query'
 
-const Section: React.FC<{
-  title: string
-}> = ({children, title}) => {
-  const viewRef = useRef<View>(null)
-  const viewOpacity = useRef<Animated.Value>(new Animated.Value(1)).current
+const queryClient = new QueryClient()
 
-  const isDarkMode = useColorScheme() === 'dark'
-  return (
-    <View ref={viewRef} style={styles.sectionContainer}>
-      <Button
-        title="set opacity to 0"
-        onPress={() => {
-          Animated.timing(viewOpacity, {
-            toValue: 0,
-            duration: 10000,
-            useNativeDriver: true,
-          }).start()
-        }}
-      />
-      <Animated.Text
-        style={[
-          styles.sectionTitle,
-          {
-            opacity: viewOpacity,
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Animated.Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  )
+if (__DEV__) {
+  import('react-query-native-devtools').then(({addPlugin}) => {
+    console.log('[trace] react-query-native-devtools')
+    addPlugin({queryClient})
+  })
 }
 
 const App = () => {
@@ -79,53 +28,23 @@ const App = () => {
   }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={backgroundStyle}>
+          <Header />
+          <View
+            style={{
+              backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            }}>
+            <Animation />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </QueryClientProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-})
 
 export default App
